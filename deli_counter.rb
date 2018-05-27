905df8e51ba7d6e2d282281c3fa98ba4a70bad9c
Stephen F <stephen@farver.io>
2018-05-27 21:50:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.size > 0 
+    puts array
+  else
+    puts "The line is currently empty."
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "#{name}, #{katz_deli.length}"
+end
+
+def now_serving
+  if katz_deli.length > 0 
+    puts katz_deli.first 
+    katz_deli
+  
