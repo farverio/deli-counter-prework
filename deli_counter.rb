7cfe8a13320e2cc6dc803abed553712246bfa6da
Stephen F <stephen@farver.io>
2018-05-27 21:51:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,8 +12,14 @@ def take_a_number(katz_deli, name)
   puts "#{name}, #{katz_deli.length}"
 end
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli.length > 0 
     puts katz_deli.first 
-    katz_deli
+    katz_deli.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
+
+
   
